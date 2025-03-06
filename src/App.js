e65840cd15e444ac81bc598363dc49cc9941bdc4
Pedro Alves <140030913+pedroca46@users.noreply.github.com>
2025-03-06 13:25:51 -0300

    Add Gsap Framework, animations, etc.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { motion } from 'framer-motion';
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 import CardServices from './components/cardServices/CardServices';
 import Navbar from './components/navbar/Navbar';
@@ -19,6 +21,46 @@ import { faHandshake } from '@fortawesome/free-solid-svg-icons';
 
 
 function App() {
+
+  const noLimitless_p = useRef(null)
+  const noLimitless_h1 = useRef(null)
+  const noLimitless_mailto = useRef(null)
+  const noLimitless = useRef(null)
+
+  gsap.registerPlugin(ScrollTrigger)
+
+  useEffect(()=>{
+    var elements = gsap.utils.toArray([noLimitless_h1.current, noLimitless_p.current, noLimitless_mailto.current]);
+    let animDel = 0
+
+    elements.forEach(el => {
+      console.log(el)
+      animDel += .1
+      gsap.fromTo(el, {opacity: 0, y: 100}, {opacity: 1, y: 0, duration: .7, delay: animDel,scrollTrigger: {
+        trigger: noLimitless.current,
+        start: "top 80%"
+      }})
+    }) 
+  })
+
+  // var split = new SplitText((noLimitless_h1, noLimitless_p) =>{
+  //   gsap.from((noLimitless_h1, noLimitless_p), {
+  //     duration: 0.8,
+  //     opacity: 0,
+  //     scale: 0,
+  //     y: 80,
+  //     rotationX: 180,
+  //     transformOrigin: "0% 50% -50",
+  //     ease: "back",
+  //     stagger: 0.01
+  //   });
+  // });
+
+
+
+
+
+
   return (
     <>
       <div className='App'>
@@ -32,11 +74,11 @@ function App() {
             de suprimentos global.</p>
             <a href="#" className='header-contactButton'>Contate-nos</a>
           </div>
-          <motion.div className='header-learnMore'>
+          <div className='header-learnMore'>
             <FontAwesomeIcon icon={faArrowDown}  style={{color: "#ffffff",}} className='iconArrowDown' />
             <h1 className='header-text'>Quais são nossos serviços?</h1>
             <FontAwesomeIcon icon={faArrowDown}  style={{color: "#ffffff",}} className='iconArrowDown' /> {/* bounce */}
-          </motion.div>
+          </div>
 
         </div>
 
@@ -75,15 +117,15 @@ function App() {
           </div>
         </div>
         
-        <div className='section-noLimitless'>
+        <div className='section-noLimitless' ref={noLimitless}>
             <div className='section-noLimitless-mainContent'>
               <img src={ag1} alt='Foto'></img>
               <div className='section-noLimitless-textContainer'>
-                <h1>Sem limites</h1>
-                <p>Rompa seus limites de compra e venda, acabe com as fronteiras que separam seus negócios de clientes e fornecedores mundiais.
+                <h1 ref={noLimitless_h1}>Sem limites</h1>
+                <p ref={noLimitless_p}>Rompa seus limites de compra e venda, acabe com as fronteiras que separam seus negócios de clientes e fornecedores mundiais.
                 Acompanhamos você e sua empresa nesse processo de evolução,
                 com toda clareza fundamentada na reputação construída ao longo dos anos.</p>
-                <a href="mailto:pedro.alves6491@gmail.com">Entre em contato pelo info@andregraize.com.br</a>
+                <a href="mailto:pedro.alves6491@gmail.com" ref={noLimitless_mailto}>Entre em contato pelo info@andregraize.com.br</a>
               </div>
             </div>
         </div>
